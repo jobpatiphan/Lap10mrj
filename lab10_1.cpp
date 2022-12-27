5e0383b69fb960c188fd9bf07e366157ed2d0abb
mrj <patiphan_klinhom@cmu.ac.th>
2022-12-27 16:04:15 +0700

    Lap completed!!!

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,17 @@
 using namespace std;
 
 int main(){	
+	double loan, rate, payment;
+	double newbalance;
+	double interest, total;
+	int i=1;
+	double prevbalance;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> payment;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +28,25 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	newbalance = loan;
+	do{
+	prevbalance = newbalance;
+	interest = newbalance * rate/100;
+	total = newbalance + interest;
+	newbalance = total - payment;
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << prevbalance;
+	cout << setw(13) << left << interest;
+	if(total<payment){
+		payment = total;
+		newbalance = 0;
+	}
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << payment;
+	cout << setw(13) << left << newbalance;
 	cout << "\n";	
-	
+	i++;
+	}while(newbalance>0);
 	return 0;
 }
